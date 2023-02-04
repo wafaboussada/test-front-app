ef20f13b6d36b769b7876f271658dff5693a769d
wafaboussada <wafaboussada@gmail.com>
2023-02-04 16:30:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [age, setAge] = useState(undefined);
+  const [year, setYear] = useState('');
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(false);
+  const handleChange = (e) => {
+    const currentYear = new Date().getFullYear();
+    const re = /^[0-9\b]+$/;
+      if (e.value === '' ||(e.value.length <=4
+        && Number(e.value) <= currentYear
+        && re.test(e.value))) {
+        setYear(e.value)
+      }
+  }
+  const sumbitForm = (e) => {
+    const headers = {
+      "Content-Type": "application/json"
+  }
+    e.preventDefault()
+    axios.post('http://localhost:7000/calculate_age', { year })
+    .then((response) => {
+      setOpen(true);
+      setAge(response.data.age);
+    })
+    .catch(function (error) {
+      setError(true);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={sumbitForm} className="form">
+        <label>Année</label>
+        <input 
+        onChange={(e) => handleChange(e.target)}
+        value={year}
+        type="text" 
+        placeholder="Entrez Votre Année ..."
+        pattern="[0-9]{1,4}"
+        />
+        <button className="button">Submit</button>
+        {open && (
+          <div>Your age is {age}</div>
+        )}
+        {error && (
+          <div style={{color: 'lightcoral'}}>ERROR ! Please try agin</div>
+        )}
+      </form>
     </div>
   );
 }
